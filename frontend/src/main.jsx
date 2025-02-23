ac9a7b13afd80b1bd8d4ca489112e9f60d26fbed
jainethin <jainethin59@gmail.com>
2025-02-23 17:31:23 +0530

    deploy

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import './index.css';
 import {ApolloClient , InMemoryCache , ApolloProvider} from '@apollo/client'
 
 const client = new ApolloClient({
-  uri : "http://localhost:4000/graphql",
+  uri : "https://redblueorders.onrender.com/graphql",
   credentials : "include",
   cache : new InMemoryCache(),
 })
